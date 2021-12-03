d18a828b2f4590f939010c7282325170da6e7c58
Afsal ms <afsalms715@gmail.com>
2021-12-03 20:01:27 +0530

    update and create done

--- tudoapp/urls.py
+++ tudoapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.fun,name='fun'),
-    path('update/',views.update,name='update')
+    path('',views.fun,name='home'),
+    path('update/<int:todu_id>/',views.update,name='update'),
+    path('delete/<int:todu_id>/',views.delete,name='delete')
 ]
--- tudoapp/views.py
+++ tudoapp/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from tudoapp.forms import toduModel
+from tudoapp.models import tudo
 
 
 def fun(request):
     form=toduModel()
-    return render(request,'home.html',{'form':form})
+    todus=tudo.objects.all()
+    if request.method == 'POST':
+        form=toduModel(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request,'home.html',{'form':form,'todus':todus})
 
-def update(request):
-    return render(request,'update.html')
+def update(request,todu_id):
+    tudoss=tudo.objects.get(id=todu_id)
+    form = toduModel(instance=tudoss)
+    if request.method == 'POST':
+        form=toduModel(request.POST,instance=tudoss)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request,'update.html',{'form':form})
+
+def delete(request,todu_id):
+    if request.method == 'POST':
+        tudo.objects.get(id=todu_id).delete()
+        return redirect('home')
